3fd5ffb902ef208a0fce0ae59e1dc73494436a3d
Chris Martin <ch.martin@gmail.com>
2015-09-14 23:02:38 -0700

    Add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+set --erase fish_greeting
+
+set PATH $HOME/bin $HOME/.cabal/bin $PATH
+
+set -x ANDROID_HOME $HOME/lib/android
+
+alias emacs '/bin/emacs -nw'
+
+# https://twitter.com/chris__martin/status/420992421673988096
+alias such git
+alias very git
+alias wow 'git status'
