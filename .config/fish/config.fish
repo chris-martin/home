f945d5eb277ef0f761a48d6b934c723bc4e4a283
Chris Martin <ch.martin@gmail.com>
2015-09-15 02:06:13 -0700

    Set EDITOR=vim

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,8 @@ set PATH $HOME/bin $HOME/.cabal/bin $PATH
 
 set -x ANDROID_HOME $HOME/lib/android
 
+set EDITOR vim
+
 alias emacs '/bin/emacs -nw'
 
 # https://twitter.com/chris__martin/status/420992421673988096
