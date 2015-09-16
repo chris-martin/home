46d7167fa215f2520b5fa00d3d34e8bed14d527f
Chris Martin <ch.martin@gmail.com>
2015-09-15 17:23:20 -0700

    Fix emacs alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,7 +6,7 @@ set -x ANDROID_HOME $HOME/lib/android
 
 set EDITOR vim
 
-alias emacs '/bin/emacs -nw'
+alias emacs '/usr/bin/env emacs -nw'
 
 # https://twitter.com/chris__martin/status/420992421673988096
 alias such git
