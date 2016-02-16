996cf272f723fae38deb4b3ff3a16467c28bc8ee
Chris Martin <ch.martin@gmail.com>
2016-02-16 03:33:58 -0800

    dconf: more terminal settings

--- .config/dconf/setup.sh
+++ .config/dconf/setup.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/use-custom-command true
+dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/use-system-font false
+dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/font "'Monospace 11'"
+dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/cursor-shape "'ibeam'"
 dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/custom-command "'tmux'"
 dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/scrollbar-policy "'never'"
+dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/audible-bell false
+dconf write /org/gnome/terminal/legacy/default-show-menubar false
 dconf write /org/gnome/desktop/peripherals/keyboard/repeat-interval 'uint32 20'
 dconf write /org/gnome/desktop/peripherals/keyboard/delay 'uint32 300'
 dconf write /org/gnome/desktop/wm/preferences/mouse-button-modifier "'<Alt>'"
