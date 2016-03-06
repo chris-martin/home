fc819296ef6e01507242d39bf778c17cf771ee68
Chris Martin <ch.martin@gmail.com>
2016-03-05 19:58:52 -0800

    fish: FISH_PROMPT env var

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,15 @@
 set --erase fish_greeting
 
+functions --copy fish_prompt default_fish_prompt
+
+function fish_prompt
+  if [ $FISH_PROMPT ]
+    echo "$FISH_PROMPT"
+  else
+    default_fish_prompt
+  end
+end
+
 set HOSTNAME (hostname)
 
 set PATH                 \
