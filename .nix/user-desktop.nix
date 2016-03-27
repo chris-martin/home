1b23885f2079fec3a7903256d0eae9ed56ed7ff3
Chris Martin <ch.martin@gmail.com>
2016-03-26 20:04:17 -0700

    add moc and pithos

--- .nix/user-desktop.nix
+++ .nix/user-desktop.nix
@@ -63,6 +63,8 @@ with rec {
 
       texLiveFull                        # LaTeX
 
+      moc pithos                         # Music
+
       nmap                               # Networking
 
       nix-repl                           # Nix
