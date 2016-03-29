85a132bef72c1e9815e897988fe3e945722363e9
Chris Martin <ch.martin@gmail.com>
2016-03-29 14:58:31 -0700

    nix: idea 15.0.4 -> 15.0.5

--- .nix/idea/default.nix
+++ .nix/idea/default.nix
@@ -33,13 +33,13 @@ in
 
   idea15-ultimate = buildIdea rec {
     name = "idea-ultimate-${version}";
-    version = "15.0.4";
-    build = "IU-143.2287";
+    version = "15.0.5";
+    build = "IU-143.2332.3";
     description = "Integrated Development Environment (IDE) by Jetbrains, requires paid license";
     license = stdenv.lib.licenses.unfree;
     src = fetchurl {
       url = "https://download.jetbrains.com/idea/ideaIU-${version}.tar.gz";
-      sha256 = "0416y7krrak1q5pb8axskdamy06nfxmn4hj7421j8jaz0nc50dn4";
+      sha256 = "1hvc5cmbfpp0qad0236ffh3g7zwfk64rh5bkkb750h3387jz7nr2";
     };
   };
 
