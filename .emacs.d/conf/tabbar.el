826888a62e3718c37f2420bba9eb89b338ad08f0
Chris Martin <ch.martin@gmail.com>
2016-04-03 00:39:54 -0700

    emacs: memoize tabbar-buffer-groups-function
    https://github.com/dholm/tabbar/issues/25

--- .emacs.d/conf/tabbar.el
+++ .emacs.d/conf/tabbar.el
@@ -7,22 +7,38 @@
 
 (defun find-tabbar-group-dir (dir)
   "Search up the directory tree looking for a tabbar group root."
-  (cond
-   ((not dir)                                          "emacs")
-   ((string-equal "*" (substring (buffer-name) 0 1))   "emacs")
-   ((string= dir "/")                                  "no-group")
+  (let* ((dir (expand-file-name dir)))
+    (cond
+     ((not dir)                                          "emacs")
+     ((string-equal "*" (substring (buffer-name) 0 1))   "emacs")
+     ((string= dir "/")                                  "no-group")
 
-   ((file-exists-p (concat dir "/.git"))            (directory-file-name dir))
-   ((file-exists-p (concat dir "/.projectile"))     (directory-file-name dir))
+     ((file-exists-p (concat dir "/.git"))            (directory-file-name dir))
+     ((file-exists-p (concat dir "/.projectile"))     (directory-file-name dir))
 
-   (t (find-tabbar-group-dir (directory-file-name (file-name-directory dir))))))
+     (t (find-tabbar-group-dir (directory-file-name (file-name-directory dir)))))))
 
-;; Set the tabbar grouping function to group by project.
+;; Cached data for the tab group function. This never gets cleaned up, so it
+;; does leak memory, but hopefully not significant unless we open an absurd
+;; number of buffers. We could probably do this more intelligently by periodically
+;; checking the complete list of buffers and cleanng up ones that no longer exist,
+;; or just by periodically flushing the whole cache.
+(defconst group-table (make-hash-table))
+
+;; Return (f x) and store the result in the table, or use an existing value
+;; from the table if it's there. We're not using the skeeto's memoize.el for
+;; this, because it isn't fast enough.
+(defun cache (f x table)
+  (or (gethash x table)
+      (let ((y (funcall f x)))
+        (progn (puthash x y table) y))))
+
+;; We memoize this function because it's called often and requires disk access,
+;; which is noticeably slow if some files are under network mount.
 (setq
  tabbar-buffer-groups-function
  (lambda ()
-   (list
-     (find-tabbar-group-dir (expand-file-name default-directory)))))
+   (list (cache 'find-tabbar-group-dir default-directory group-table))))
 
 (setq tabbar-use-images nil)
 
