a91f10885eb8ecdc88598e406f68f0caca2b43fc
Chris Martin <ch.martin@gmail.com>
2016-04-02 22:34:43 -0700

    nicer emacs tab styling

--- .emacs.d/conf/tabbar.el
+++ .emacs.d/conf/tabbar.el
@@ -1,3 +1,5 @@
+(require 'tabbar)
+
 (add-hook 'after-init-hook 'tabbar-mode)
 
 (global-set-key [M-left] 'tabbar-backward-tab)
@@ -15,8 +17,32 @@
 
    (t (find-tabbar-group-dir (directory-file-name (file-name-directory dir))))))
 
+;; Set the tabbar grouping function to group by project.
 (setq
  tabbar-buffer-groups-function
  (lambda ()
    (list
      (find-tabbar-group-dir (expand-file-name default-directory)))))
+
+(setq tabbar-use-images nil)
+
+(defun set-tabbar-face (face fg bg)
+  (set-face-attribute face nil
+                      :background bg
+                      :foreground fg
+                      :box `(:line-width 4 :color ,bg :style nil)))
+
+(set-tabbar-face 'tabbar-default    "gray10" "#ddd")
+(set-tabbar-face 'tabbar-unselected "gray10" "#ddd")
+(set-tabbar-face 'tabbar-selected   "black"  "gray65")
+(set-tabbar-face 'tabbar-modified   "blue"   "#ddd")
+(set-tabbar-face 'tabbar-highlight  "black"  "#gray75")
+(set-tabbar-face 'tabbar-button     "gray10" "#ddd")
+(set-tabbar-face 'tabbar-separator  "black"  "#ddd")
+
+(set-face-attribute 'tabbar-separator nil :height 165)
+
+(tabbar-mode 1)
+
+(setq tabbar-tab-label-function
+      (lambda (tab) (format "   %s    " (tabbar-buffer-tab-label tab))))
