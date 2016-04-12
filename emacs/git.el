f72b3a5b07508b06498d9c2a5daa2fa6f491c013
Chris Martin <ch.martin@gmail.com>
2016-04-11 21:06:22 -0700

    emacs: diff-hl-mode

--- emacs/git.el
+++ emacs/git.el
@@ -2,3 +2,6 @@
 (package-initialize) ; otherwise Emacs doesn't know were to find it
 (setq package-initialize-at-startup nil) ; don't do it again
 (global-git-commit-mode)
+
+;; Highlight modified lines in the fringe
+(global-diff-hl-mode)
