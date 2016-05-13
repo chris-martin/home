c2ac812b463210ab8ef3a53787ab4e06d84b4f80
Chris Martin <ch.martin@gmail.com>
2016-05-13 14:16:29 -0400

    emacs: custom.el

--- emacs/custom.el
+++ emacs/custom.el
@@ -1,13 +1,21 @@
 (custom-set-faces
- '(fringe ((t (:background "#f2f2f2"))))
- '(hl-line ((t (:background "#fffae3"))))
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(flycheck-error ((t (:foreground "#c00" :underline "#cc0000"))))
- '(flycheck-warning ((t (:underline "purple"))))
  '(flycheck-fringe-error ((t :foreground "#c00" :background "#efe3e3")))
  '(flycheck-fringe-warning ((t (:foreground "purple" :background "#ece0ec"))))
- )
+ '(flycheck-warning ((t (:underline "purple"))))
+ '(fringe ((t (:background "#f2f2f2"))))
+ '(hl-line ((t (:background "#fffae3")))))
 
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cwm-fringe-background "#f2f2f2")
+ '(send-mail-function (quote mailclient-send-it))
  '(tab-stop-list (number-sequence 4 200 4))
- '(warning-suppress-types (quote ((undo discard-info))))
- )
+ '(warning-suppress-types (quote ((undo discard-info)))))
