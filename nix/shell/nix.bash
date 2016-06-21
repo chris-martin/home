a706d6130abcbf5f8e9e1a7bda66e613897c5c51
Chris Martin <ch.martin@gmail.com>
2016-06-21 15:30:10 -0400

    nix: add nixos-unstable to NIX_PATH for bash

--- nix/shell/nix.bash
+++ nix/shell/nix.bash
@@ -2,4 +2,5 @@ export NIXPKGS_CONFIG="$HOME/nix/config.nix"
 
 export NIX_PATH="\
 nixpkgs=$HOME/.nix-defexpr/channels/nixos-16.03:\
+nixpkgs-unstable=$HOME/.nix-defexpr/channels/nixos-unstable:\
 nixos-config=$HOME/nix/os/config.nix"
