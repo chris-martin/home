97a4ace3a085da2e9e740a14a8bfb0a9e5910321
Chris Martin <ch.martin@gmail.com>
2016-09-01 10:22:12 -0400

    nix: commented-out displaylink video driver
    https://github.com/NixOS/nixpkgs/pull/18041

--- nix/os/boxes/renzo.nix
+++ nix/os/boxes/renzo.nix
@@ -38,6 +38,8 @@
     accelFactor = "0.05";
   };
 
+  #services.xserver.videoDrivers = [ "displaylink" "modesetting" ];
+
   environment.etc."modprobe.d/alsa-base.conf".text = ''
     options snd-hda-intel index=1 model=dell-headset-multi
     options snd-hda-intel index=0 model=auto vid=8086 pid=9d70
