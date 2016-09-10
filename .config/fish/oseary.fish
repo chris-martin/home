de853e6f4d308e83b1c8823376580d9e79c69ad4
Chris Martin <ch.martin@gmail.com>
2016-09-09 23:11:56 -0400

    fish: consolidate oseary into 1 fn w/ completion

--- .config/fish/oseary.fish
+++ .config/fish/oseary.fish
@@ -1,8 +1,32 @@
-function oseary-mount
-  env USER=chris PASSWORD=(cat ~/secrets/oseary) \
-    HOST=192.168.0.100 MOUNT=$HOME/mnt/oseary sshfs-mount
+function oseary --argument command
+  switch $command
+
+  case mount
+    env USER=chris PASSWORD=(cat ~/secrets/oseary) \
+      HOST=192.168.0.100 MOUNT=$HOME/mnt/oseary sshfs-mount
+
+  case umount
+    fusermount -u $HOME/mnt/oseary; and echo "Unmounted."
+
+  case '*'
+    echo "???"
+
+  end
 end
 
-function oseary-umount
-  fusermount -u $HOME/mnt/oseary
+function __fish_oseary_needs_command
+  set cmd (commandline -opc)
+  if [ (count $cmd) -eq 1 ]
+    return 0
+  else
+    return 1
+  end
 end
+
+complete -f -c oseary -a mount \
+  -n __fish_oseary_needs_command \
+  -d "Mount the NAS"
+
+complete -f -c oseary -a umount \
+  -n __fish_oseary_needs_command \
+  -d "Unmount the NAS"
