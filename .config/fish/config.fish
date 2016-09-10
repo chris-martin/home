788857bea927f96c6a98148f742c80c8b69b9ca2
Chris Martin <ch.martin@gmail.com>
2016-09-09 22:03:03 -0400

    fish: tab completion for git aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -32,28 +32,40 @@ function oseary-umount
   fusermount -u $HOME/mnt/oseary
 end
 
-# https://twitter.com/chris__martin/status/420992421673988096
-alias such git
-alias very git
-alias wow 'git status'
+function alias2 --argument alias command
+  alias "$alias" "$command"
 
-alias add    'git add'
-alias amend  'git commit --amend'
-alias staged 'git diff --cached'
-alias cherry 'git cherry-pick'
-alias ci     'git commit'
-alias clone  'git clone'
-alias co     'git checkout'
-alias gdiff  'git diff'
-alias fetch  'git fetch'
-alias log    'git log'
-alias merge  'git merge'
-alias pull   'git pull'
-alias push   'git push'
-alias rebase 'git rebase'
-alias revert 'git revert'
-alias show   'git show'
-alias stash  'git stash'
+  # Set tab completion for the alias that works like the full command
+  # https://github.com/fish-shell/fish-shell/issues/393#issuecomment-12689588
+  complete -c $alias -a "(
+    set -l cmd (commandline -op);
+    set -e cmd[1];
+    complete -C\"$command \$cmd\";
+  )"
+end
+
+# https://twitter.com/chris__martin/status/420992421673988096
+alias2 such git
+alias2 very git
+alias2 wow st
+
+alias2 add    'git add'
+alias2 amend  'git commit --amend'
+alias2 staged 'git diff --cached'
+alias2 cherry 'git cherry-pick'
+alias2 ci     'git commit'
+alias2 clone  'git clone'
+alias2 co     'git checkout'
+alias2 gdiff  'git diff'
+alias2 fetch  'git fetch'
+alias2 log    'git log'
+alias2 merge  'git merge'
+alias2 pull   'git pull'
+alias2 push   'git push'
+alias2 rebase 'git rebase'
+alias2 revert 'git revert'
+alias2 show   'git show'
+alias2 stash  'git stash'
 
 # https://github.com/fish-shell/fish-shell/issues/838
 function readSilent # prompt targetVar
