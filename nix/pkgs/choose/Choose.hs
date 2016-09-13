589e02777adc24da879e943e76b1afa0539f158f
Chris Martin <ch.martin@gmail.com>
2016-09-12 20:48:03 -0400

    choose: move algo doc to Choose.Tree

--- nix/pkgs/choose/Choose.hs
+++ nix/pkgs/choose/Choose.hs
@@ -3,20 +3,7 @@
 -- | Reads lines from stdin and outputs N of them, selected uniformly at
 -- random.
 --
--- * Algorithm outline
---
--- We store items on a binary tree, moving them down the left or right branch
--- according to a coin flip. At the end, the rightmost N items on the tree are
--- selected. Each time we insert into a tree having N items, we prune its
--- leftmost item.
---
--- It may be helpful to think of this as lazily assigning each item a d-bit
--- score, where d is the maximum depth of the tree. Moving an item to the
--- left or right corresponds to appending a 0 or 1 to its score. Note that
--- the laziness is necessary, because d is not known a priori.
---
--- The process of moving items futher down the tree is herein referred to as
--- "disambiguation", because its purpose is to resolve ties in the score.
+-- The algorithm is described in "Choose.Tree".
 
 module Main (main) where
 
