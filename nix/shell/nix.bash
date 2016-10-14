00d03cbd8668830351ee9d5bd0c970e82543b03c
Chris Martin <ch.martin@gmail.com>
2016-10-14 10:56:58 -0400

    nix: fix bash env vars

--- nix/shell/nix.bash
+++ nix/shell/nix.bash
@@ -4,13 +4,13 @@ NIXOS_CONFIG="$HOME/nix/os/config.nix"
 
 CHANNELS="$HOME/nix/channels"
 
-STABLE="$CHANNELS/nixos-16.09"
+STABLE="$CHANNELS/16.09"
 
 NIX_PATH_BASE="\
 nixpkgs-master=$CHANNELS/master:\
-nixpkgs-unstable=$CHANNELS/nixos-unstable:\
-nixpkgs-16.09=$CHANNELS/nixos-16.09:\
-nixpkgs-16.03=$CHANNELS/nixos-16.03:\
+nixpkgs-unstable=$CHANNELS/unstable:\
+nixpkgs-16.09=$CHANNELS/16.09:\
+nixpkgs-16.03=$CHANNELS/16.03:\
 nixos-config=$NIXOS_CONFIG"
 
 export NIX_PATH="nixpkgs=$STABLE:$NIX_PATH_BASE"
