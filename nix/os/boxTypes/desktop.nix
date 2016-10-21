1b6ff7423af91e749930e03288d0e737454a5b89
Chris Martin <ch.martin@gmail.com>
2016-10-21 14:39:03 -0400

    use opendns

--- nix/os/boxTypes/desktop.nix
+++ nix/os/boxTypes/desktop.nix
@@ -18,7 +18,8 @@
 
   networking = {
     networkmanager.enable = true;
-    nameservers = [ "8.8.8.8" "8.8.4.4" ];
+    #nameservers = [ "8.8.8.8" "8.8.4.4" ];
+    nameservers = [ "208.67.222.222" "208.67.220.220" ];
     firewall.allowPing = true;
     firewall.allowedTCPPorts = [
       51413 # bittorrent
