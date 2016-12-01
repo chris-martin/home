0d685e87c34cbb40355135657bb1ac271f216d1f
Chris Martin <ch.martin@gmail.com>
2016-12-01 17:44:57 -0500

    stack: set temp dir /tmp

--- .stack/shell.nix
+++ .stack/shell.nix
@@ -6,5 +6,10 @@ haskell.lib.buildStackProject {
   name = "default-stack-shell";
   inherit ghc;
   buildInputs = [ zlib ];
+
+  # https://github.com/commercialhaskell/stack/issues/2358
   LANG = "en_US.UTF-8";
+
+  # https://github.com/commercialhaskell/stack/commit/568938da
+  TMPDIR = "/tmp";
 }
