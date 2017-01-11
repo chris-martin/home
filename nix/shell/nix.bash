42ba098d5ec2bffdbac683c6d293b2d07b363f5c
Chris Martin <ch.martin@gmail.com>
2017-01-10 17:40:55 -0700

    nix: update bash env setup

--- nix/shell/nix.bash
+++ nix/shell/nix.bash
@@ -2,17 +2,6 @@ export NIXPKGS_CONFIG="$HOME/nix/config.nix"
 
 NIXOS_CONFIG="$HOME/nix/os/config.nix"
 
-CHANNELS="$HOME/nix/channels"
+export NIX_PATH=$HOME/nix/path
 
-STABLE="$CHANNELS/16.09"
-
-NIX_PATH_BASE="\
-nixpkgs-master=$CHANNELS/master:\
-nixpkgs-unstable=$CHANNELS/unstable:\
-nixpkgs-16.09=$CHANNELS/16.09:\
-nixpkgs-16.03=$CHANNELS/16.03:\
-nixos-config=$NIXOS_CONFIG"
-
-export NIX_PATH="nixpkgs=$STABLE:$NIX_PATH_BASE"
-
-export NIXOS_PATH="nixpkgs=$STABLE:$NIX_PATH_BASE"
+export NIXOS_PATH=$HOME/nix/path:nixos-config=$NIXOS_CONFIG
