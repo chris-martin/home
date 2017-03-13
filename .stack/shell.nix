ae0f8c09b11c0d91540ab8c94adb10b6d2e7253a
Chris Martin <ch.martin@gmail.com>
2017-03-12 23:54:26 -0400

    add latex dependency to default stack shell

--- .stack/shell.nix
+++ .stack/shell.nix
@@ -2,10 +2,18 @@
 
 with pkgs;
 
-haskell.lib.buildStackProject {
+let
+
+  latex = texlive.combine {
+    inherit (texlive)
+      scheme-medium  # An aggegrate base package with bunch of tex packages
+      ;
+  };
+
+in haskell.lib.buildStackProject {
   name = "default-stack-shell";
   inherit ghc;
-  buildInputs = [ zlib ];
+  buildInputs = [ cairo latex pango pcre zlib ];
 
   # https://github.com/commercialhaskell/stack/issues/2358
   LANG = "en_US.UTF-8";
