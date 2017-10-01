24e7299dc24812127c1068c8edd9d6fbac690c00
Chris Martin <ch.martin@gmail.com>
2017-10-01 18:21:23 -0400

    renzo: latest kernel

--- nix/os/renzo.nix
+++ nix/os/renzo.nix
@@ -230,7 +230,7 @@ in {
   #  Kernel
   #-----------------------------------------------------------------------------
 
-  boot.kernelPackages = pkgs.linuxPackages_4_4;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelModules  = ["snd-hda-intel"];
   boot.kernelParams = ["pci=nocrs"];
 
