92ce91cc03efb427932d6305f72c17418c58767e
Chris Martin <ch.martin@gmail.com>
2017-12-14 22:09:01 -0500

    renzo: disable postgres

--- nix/os/renzo.nix
+++ nix/os/renzo.nix
@@ -235,7 +235,7 @@
   #  Postgres
   #-----------------------------------------------------------------------------
 
-  services.postgresql.enable = true;
+  services.postgresql.enable = false;
   services.postgresql.package = pkgs.postgresql94;
   services.postgresql.authentication = "local all all ident";
 
