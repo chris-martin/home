fe2e575f5c79b561cd7f5f6024e5d1e424fcd72a
Chris Martin <ch.martin@gmail.com>
2018-02-17 22:50:44 -0500

    nix: add rebuild-os bash function

--- nix/shell/nix.bash
+++ nix/shell/nix.bash
@@ -1,3 +1,10 @@
 export NIX_PATH="$HOME/nix/path"
 export NIXPKGS_CONFIG="$HOME/nix/config.nix"
 export NIXOS_CONFIG="$HOME/nix/os/config.nix"
+
+function rebuild-os {
+   sudo env \
+     NIX_PATH="$NIX_PATH" \
+     NIXOS_CONFIG="$NIXOS_CONFIG" \
+     nixos-rebuild switch
+}
