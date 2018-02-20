5386cd7414fc3b5f358007dbd592bc37ea6647df
Chris Martin <ch.martin@gmail.com>
2018-02-20 17:51:33 -0500

    add some hoogle packages

--- nix/os/hoogle.nix
+++ nix/os/hoogle.nix
@@ -11,18 +11,23 @@
     p.cookie
     p.conduit-combinators
     p.containers
+    p.cryptonite
     p.data-forest
     p.directory
     p.ede
     p.either-list-functions
     p.fizzbuzz-as-a-service
     p.hedgehog
+    p.http-client
+    p.http-client-tls
+    p.http-conduit
     p.http-types
     p.human-parse
     p.human-text
     p.lens
     p.loc
     p.loc-test
+    p.mtl
     p.multi-instance
     p.mvar-lock
     p.neat-interpolation
@@ -45,13 +50,23 @@
     p.safe-exceptions
     p.scotty
     p.stm
+    p.stripe-core
+    p.stripe-haskell
+    p.stripe-http-streams
     p.text
     # p.text-replace
     p.time
+    p.transformers
     p.utf8-string
     p.var-monad
     p.vector
     p.wai
     p.warp
+    p.wreq
+    p.yi-core
+    p.yi-keymap-vim
+    p.yi-misc-modes
+    p.yi-mode-haskell
+    p.yi-mode-javascript
   ];
 }
