fbdc4091ab6aff7b31db573f30a95770a015664d
Chris Martin <ch.martin@gmail.com>
2018-02-23 03:14:30 -0500

    yi: delete line with ctrl+y

--- yi/yi.hs
+++ yi/yi.hs
@@ -1,12 +1,14 @@
 import Yi
-
+import Yi.Buffer.HighLevel
 import Yi.Config.Default.Cua            (configureCua)
 import Yi.Config.Default.HaskellMode    (configureHaskellMode)
 import Yi.Config.Default.JavaScriptMode (configureJavaScriptMode)
 import Yi.Config.Default.MiscModes      (configureMiscModes)
 import Yi.Config.Default.Vty            (configureVty)
+import Yi.Config.Simple
 import Yi.Config.Simple.Types           (ConfigM, runConfigM)
 
+import Control.Monad            (unless)
 import Control.Monad.State.Lazy (execStateT)
 import Data.List                (intersperse)
 import Lens.Micro.Platform      ((.=))
@@ -31,8 +33,26 @@ main = do
       configureMiscModes
       configureCua
       configureVty
+      globalBindKeys (ctrlCh 'y' ?>>! delLine)
       startActionsA .= openFileActions
 
   cfg <- execStateT (runConfigM config) defaultConfig
 
   startEditor cfg Nothing
+
+-- | Deletes the current line, moving the cursor as little as possible. If
+-- the cursor is on a blank line at the end of the file, this has no effect.
+
+delLine :: YiM ()
+delLine = withCurrentBuffer delLineB
+
+delLineB :: BufferM ()
+delLineB = do
+  e <- atEndB
+  unless e $ do
+    r <- inclusiveRegionB =<< regionOfB Line
+    lineDown
+    deleteRegionB r
+
+atEndB :: BufferM Bool
+atEndB = (&&) <$> atEof <*> atSol
