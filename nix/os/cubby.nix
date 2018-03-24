e3f9a205264a46e2643772da5421fb978c9c027a
Chris Martin <ch.martin@gmail.com>
2018-03-24 14:05:25 -0400

    fix audio on cubby

--- nix/os/cubby.nix
+++ nix/os/cubby.nix
@@ -184,7 +184,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.device = "/dev/sda";
   boot.kernelParams = [ "nomodeset" ];
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  #boot.kernelPackages = pkgs.linuxPackages_latest;
 
   boot.cleanTmpDir = true;
 
