b64ef6fe4c25435e798076b04f45c7cf94aea15e
Chris Martin <ch.martin@gmail.com>
2018-04-12 17:37:11 -0400

    renzo: reflex-frp binary cache

--- nix/os/renzo.nix
+++ nix/os/renzo.nix
@@ -15,6 +15,15 @@
 
   time.timeZone = "America/New_York"; # Eastern
 
+  nix.binaryCaches = [
+    "https://cache.nixos.org/"
+    "https://nixcache.reflex-frp.org"
+  ];
+
+  nix.binaryCachePublicKeys = [
+    "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
+  ];
+
 
   #-----------------------------------------------------------------------------
   #  Networking
