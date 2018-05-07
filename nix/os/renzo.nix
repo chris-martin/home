3aae068d4df83503947d3a47fa32c1a15be605f3
Chris Martin <ch.martin@gmail.com>
2018-05-07 14:38:56 -0400

    fucking with window managers on renzo

--- nix/os/renzo.nix
+++ nix/os/renzo.nix
@@ -54,21 +54,25 @@
   #services.xserver.desktopManager.gnome3.enable = true;
   #services.xserver.displayManager.gdm.enable = true;
 
+  services.xserver.desktopManager.plasma5.enable = true;
+
   #services.xserver.desktopManager.default = "none";
   services.xserver.displayManager.slim.enable = true;
   #services.xserver.windowManager.xmonad.enable = true;
   #services.xserver.windowManager.xmonad.enableContribAndExtras = true;
-  #services.xserver.windowManager.default = "xmonad";
+  #services.xserver.windowManager.default = "openbox";
 
-  services.xserver.windowManager = {
-    session = [{
-      name = "xmonad";
-      start = ''
-        my-xmonad &
-        waitPID=$!
-      '';
-    }];
-  };
+  #services.xserver.windowManager.openbox.enable = true;
+
+  #services.xserver.windowManager = {
+  #  session = [{
+  #    name = "xmonad";
+  #    start = ''
+  #      my-xmonad &
+  #      waitPID=$!
+  #    '';
+  #  }];
+  #};
 
 
   #-----------------------------------------------------------------------------
