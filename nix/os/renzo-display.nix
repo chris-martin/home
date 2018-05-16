991eb8926f362044d08f563acd809f880588f779
Chris Martin <ch.martin@gmail.com>
2018-05-15 23:26:57 -0400

    renzo gnome terminal font fira and bigger

--- nix/os/renzo-display.nix
+++ nix/os/renzo-display.nix
@@ -26,7 +26,7 @@
   #};
 
   services.xserver.displayManager.sessionCommands = ''
-    dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/font "'Monospace 12'"
+    dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/font "'Fira Mono 14'"
     dconf write /org/gnome/desktop/interface/font-name "'Cantarell 13'"
     dconf write /org/gnome/desktop/interface/document-font-name "'Lato Medium 13'"
     dconf write /org/gnome/desktop/wm/preferences/titlebar-font "'Lato Bold 13'"
