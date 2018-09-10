db201f9a00df683f34a99edd0ad3d0c6cf9cae5a
Chris Martin <ch.martin@gmail.com>
2018-09-10 18:58:00 -0400

    channel 18.09

--- nix/init-channels.sh
+++ nix/init-channels.sh
@@ -3,3 +3,4 @@
 nix-channel --add https://nixos.org/channels/nixpkgs-unstable unstable
 nix-channel --add https://nixos.org/channels/nixos-17.09 release-17.09
 nix-channel --add https://nixos.org/channels/nixos-18.03 release-18.03
+nix-channel --add https://nixos.org/channels/nixos-18.09 release-18.09
