b58aa6f0baa9c7c2b8ec049e63586bc95371a5a3
Chris Martin <ch.martin@gmail.com>
2018-11-16 14:02:32 -0500

    nixos 18.09

--- nix/os/renzo.nix
+++ nix/os/renzo.nix
@@ -46,7 +46,7 @@
 
   environment.etc."fuse.conf".text = "user_allow_other";
 
-  system.stateVersion = "18.03";
+  system.stateVersion = "18.09";
 
 
   #-----------------------------------------------------------------------------
