19a0ccf4e98d7bb7ab76bb7ca402884e1a118b24
Chris Martin <ch.martin@gmail.com>
2019-02-16 22:03:24 -0700

    switch to standard nix channel names

--- nix/init-channels.sh
+++ nix/init-channels.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-nix-channel --add https://nixos.org/channels/nixpkgs-unstable unstable
-nix-channel --add https://nixos.org/channels/nixos-17.09 release-17.09
-nix-channel --add https://nixos.org/channels/nixos-18.03 release-18.03
-nix-channel --add https://nixos.org/channels/nixos-18.09 release-18.09
+nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs-unstable
+nix-channel --add https://nixos.org/channels/nixos-18.03 nixos-18.03
+nix-channel --add https://nixos.org/channels/nixos-18.09 nixos-18.09
