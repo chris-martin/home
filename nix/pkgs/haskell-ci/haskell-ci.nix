34171a4d6102b06b841048a431e13b81e75b9079
Chris Martin <ch.martin@gmail.com>
2019-05-29 22:43:01 -0600

    switch to my fork of haskell-ci

--- nix/pkgs/haskell-ci/haskell-ci.nix
+++ nix/pkgs/haskell-ci/haskell-ci.nix
@@ -1,31 +1,34 @@
-{ mkDerivation, ansi-terminal, base, base-compat, bytestring, Cabal
-, containers, deepseq, Diff, directory, fetchgit, filepath
-, generic-lens, microlens, optparse-applicative, parsec, pretty
-, ShellCheck, stdenv, tasty, tasty-golden, transformers
+{ mkDerivation, aeson, ansi-terminal, base, base-compat, bytestring
+, Cabal, containers, deepseq, Diff, directory, exceptions, fetchgit
+, filepath, generic-lens, HsYAML, microlens, mtl
+, optparse-applicative, parsec, pretty, process, ShellCheck, stdenv
+, tasty, tasty-golden, temporary, text, transformers
+, unordered-containers
 }:
 mkDerivation {
   pname = "haskell-ci";
-  version = "0.1.0.0";
+  version = "0.3.20190521";
   src = fetchgit {
-    url = "https://github.com/haskell-CI/haskell-ci.git";
-    sha256 = "1qlr45jbvnsvawb0mc7958i0p881dkvprrwlz5nsfl38m0yd7cjv";
-    rev = "7302183ac70d3a6a4581e8adee88789b626c6b9d";
+    url = "https://github.com/chris-martin/haskell-ci";
+    sha256 = "1bk73yss6v9yhnc8a7hr8cxvvizx8iajcyf3m1likfs0j7r2b60f";
+    rev = "a42fe87c17d5b423dea6c7fd98412b16f86743f9";
     fetchSubmodules = true;
   };
-  isLibrary = true;
+  isLibrary = false;
   isExecutable = true;
   libraryHaskellDepends = [
-    base base-compat bytestring Cabal containers deepseq directory
-    filepath generic-lens microlens optparse-applicative parsec pretty
-    ShellCheck transformers
+    aeson base base-compat bytestring Cabal containers deepseq
+    directory exceptions filepath generic-lens HsYAML microlens mtl
+    optparse-applicative parsec pretty process ShellCheck temporary
+    text transformers unordered-containers
   ];
   executableHaskellDepends = [ base ];
   testHaskellDepends = [
     ansi-terminal base bytestring Diff directory filepath tasty
     tasty-golden transformers
   ];
-  doCheck = false;
-  homepage = "http://haskell-ci.rtfd.org/";
+  doHaddock = false;
+  homepage = "https://haskell-ci.rtfd.org/";
   description = "Cabal package script generator for Travis-CI";
   license = stdenv.lib.licenses.bsd3;
 }
