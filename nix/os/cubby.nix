504d255d620257dedcee085d9c1bc85ed7e25733
Chris Martin <ch.martin@gmail.com>
2019-06-02 01:25:04 -0600

    fucking around with monero

--- nix/os/cubby.nix
+++ nix/os/cubby.nix
@@ -7,6 +7,13 @@
 {
   nixpkgs.config = import ../config.nix;
 
+  services.monero = {
+    #enable = true;
+    #mining.enable = true;
+    mining.address = "427YsNgWdfJ9VraWsLC6h4Rygqq1VozD8Q6JC2DoPcHzbRxo6xjtWHyF7B1PBYsqFN8R37itSYthm6xiaDxGoFdFLKnDMn7";
+    mining.threads = 1;
+  };
+
   imports = [
     ./hardware.nix
     ./essentials.nix
