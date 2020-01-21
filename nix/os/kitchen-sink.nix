b2f2ade4bc27c243c4f7ad43c25d32d420972e08
Chris Martin <ch.martin@gmail.com>
2020-01-21 15:10:59 -0700

    add feedreader

--- nix/os/kitchen-sink.nix
+++ nix/os/kitchen-sink.nix
@@ -34,6 +34,7 @@
     pkgs.eog # "eye of gnome" image viewer
     pkgs.evince # PDF viewer
     pkgs.exercism
+    pkgs.feedreader
     pkgs.ffmpeg
     pkgs.ffmpegthumbnailer # enables video thumbnails in Nautilus
     pkgs.file-roller # zip GUI
