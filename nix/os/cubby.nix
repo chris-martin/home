93cdfab2c3cdb728e02b60ae5eaff8e9e28bb730
Chris Martin <ch.martin@gmail.com>
2020-10-27 15:01:00 -0600

    flip mouse buttons left/right

--- nix/os/cubby.nix
+++ nix/os/cubby.nix
@@ -72,6 +72,8 @@
 
   services.dictd.enable = true;
 
+  services.xserver.libinput.buttonMapping = "3 2 1 4 5 6 7 0 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32";
+
   environment.etc."fuse.conf".text = ''
     user_allow_other
   '';
