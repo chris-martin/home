6b7e856a22ce2b7deb2a7c6f4c9ab675d6e191e6
Chris Martin <ch.martin@gmail.com>
2020-10-27 14:38:48 -0600

    nix path updates for nixos-20.09

--- nix/init-channels.sh
+++ nix/init-channels.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
 nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs-unstable
-nix-channel --add https://nixos.org/channels/nixos-18.09 nixos-18.09
-nix-channel --add https://nixos.org/channels/nixos-19.03 nixos-19.03
-nix-channel --add https://nixos.org/channels/nixos-19.09 nixos-19.09
 nix-channel --add https://nixos.org/channels/nixos-20.03 nixos-20.03
+nix-channel --add https://nixos.org/channels/nixos-20.09 nixos-20.09
