97de5c5f43188b4c828ca9cfd3b08d57d559cbdf
Chris Martin <ch.martin@gmail.com>
2020-10-27 14:51:20 -0600

    add back nixos-18.09 for yi

--- nix/init-channels.sh
+++ nix/init-channels.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs-unstable
+nix-channel --add https://nixos.org/channels/nixos-18.09 nixos-18.09
 nix-channel --add https://nixos.org/channels/nixos-20.03 nixos-20.03
 nix-channel --add https://nixos.org/channels/nixos-20.09 nixos-20.09
