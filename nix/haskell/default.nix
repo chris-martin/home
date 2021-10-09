3d8a9f6def900b70f82a7e7dbad2a492abd625e6
Chris Martin <ch.martin@gmail.com>
2021-10-09 01:58:40 -0600

    bump ghc minor to 8.10.7

--- nix/haskell/default.nix
+++ nix/haskell/default.nix
@@ -2,7 +2,7 @@
 
 let
 
-  compilerVersion = "ghc8106";
+  compilerVersion = "ghc8107";
   packages = import ./packages.nix;
   haskellPackages = haskell.packages.${compilerVersion}.override { overrides = import ./overrides.nix; };
 
