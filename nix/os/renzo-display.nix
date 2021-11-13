39ea23b3f8a1c443e08ba5b9d19c9d7d2cc5b815
Chris Martin <ch.martin@gmail.com>
2021-11-13 01:07:41 -0700

    Revert "switch renzo to plasma+wayland"
    This reverts commit 8cbb7f4ec74c641d961aff8bb6c5c81e9d11814c.

--- nix/os/renzo-display.nix
+++ nix/os/renzo-display.nix
@@ -1,15 +1,23 @@
-{ pkgs, ... }:
+{ ... }:
 {
-  services.xserver = {
-    enable = true;
-    displayManager = {
-      sessionPackages = [
-        (pkgs.plasma-workspace.overrideAttrs (old: { passthru.providedSessions = ["plasmawayland"]; }))
-      ];
-    };
-    desktopManager.plasma5 = {
-      enable = true;
-      #runUsingSystemd = true;
-    };
-  };
+  services.xserver.enable = true;
+
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+
+  #services.xserver.desktopManager.plasma5.enable = true;
+
+  #services.xserver.desktopManager.default = "none";
+  #services.xserver.displayManager.slim.enable = true;
+  #services.xserver.windowManager.default = "openbox";
+
+  #services.xserver.windowManager.openbox.enable = true;
+/*
+  services.xserver.displayManager.sessionCommands = ''
+    dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/font "'Fira Mono 14'"
+    dconf write /org/gnome/desktop/interface/font-name "'Cantarell 13'"
+    dconf write /org/gnome/desktop/interface/document-font-name "'Lato Medium 13'"
+    dconf write /org/gnome/desktop/wm/preferences/titlebar-font "'Lato Bold 13'"
+  '';
+*/
 }
