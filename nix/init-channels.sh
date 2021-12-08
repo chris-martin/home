e3fc6c8750291a76fa69cea695344d0458b15fc1
Chris Martin <ch.martin@gmail.com>
2021-12-07 22:10:29 -0700

    nixos 21.11

--- nix/init-channels.sh
+++ nix/init-channels.sh
@@ -5,3 +5,4 @@ nix-channel --add https://nixos.org/channels/nixos-18.09 nixos-18.09
 nix-channel --add https://nixos.org/channels/nixos-20.03 nixos-20.03
 nix-channel --add https://nixos.org/channels/nixos-20.09 nixos-20.09
 nix-channel --add https://nixos.org/channels/nixos-21.05 nixos-21.05
+nix-channel --add https://nixos.org/channels/nixos-21.11 nixos-21.11
