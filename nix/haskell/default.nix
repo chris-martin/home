4048b4a5d664ef7514aa1619a4cbfc40b99dab9f
Chris Martin <ch.martin@gmail.com>
2021-12-09 01:33:15 -0700

    remove unused ghcide line

--- nix/haskell/default.nix
+++ nix/haskell/default.nix
@@ -11,5 +11,4 @@ in
 {
   ghc = haskellPackages.ghcWithPackages (p: map (name: p.${name}) packages);
   ghcid = haskellPackages.ghcid;
-  ghcide = (import (builtins.fetchTarball "https://github.com/hercules-ci/ghcide-nix/tarball/fd42f62613d491565c1676821148c008b2011584") {})."ghcide-${compilerVersion}";
 }
