db004a9e514b1852da3ef8245995bdfdf66926bd
Chris Martin <ch.martin@gmail.com>
2021-12-31 01:26:16 -0700

    trim kitchen sink

--- nix/os/kitchen-sink.nix
+++ nix/os/kitchen-sink.nix
@@ -6,9 +6,9 @@
     pkgs.alock
     pkgs.alsaUtils
     #pkgs.alsamixer # audio controls
-    pkgs.android-udev-rules
+    #pkgs.android-udev-rules
     pkgs.ansifilter
-    pkgs.audacity
+    #pkgs.audacity
     pkgs.arcanist
     pkgs.astyle
     pkgs.atom # text editor
@@ -19,28 +19,28 @@
     pkgs.cabal2nix
     pkgs.cachix
     pkgs.calibre
-    pkgs.cheese # Camera GUI
-    pkgs.chromium
+    #pkgs.cheese # Camera GUI
+    #pkgs.chromium
     #pkgs.choose-exe # Chooses random elements from a stream
     pkgs.cryptsetup # LUKS disk encryption
     pkgs.curl
-    pkgs.discord
+    #pkgs.discord
     pkgs.dmenu
     pkgs.doctest
-    pkgs.elmPackages.elm
-    pkgs.elmPackages.elm-format
-    pkgs.entr
+    #pkgs.elmPackages.elm
+    #pkgs.elmPackages.elm-format
+    #pkgs.entr
     pkgs.eog # "eye of gnome" image viewer
     pkgs.evince # PDF viewer
-    pkgs.exercism
+    #pkgs.exercism
     pkgs.ffmpeg-full
     pkgs.ffmpegthumbnailer # enables video thumbnails in Nautilus
     pkgs.file-roller # zip GUI
     pkgs.fish # shell
-    pkgs.fontforge
+    #pkgs.fontforge
     #pkgs.FractalArt
     pkgs.fullwidth # converts strings to their fullwidth forms
-    pkgs.gcc # C compiler
+    #pkgs.gcc # C compiler
     pkgs.gnome3.dconf-editor
     pkgs.gnome3.gedit
     pkgs.ghc
@@ -56,43 +56,42 @@
     pkgs.glib.dev # provides the "gio" command
     pkgs.gmrun
     pkgs.gnome-screenshot # screenshot tool
+    #pkgs.gopher
+    #pkgs.gparted # partitioning tool
+    #pkgs.gptfdisk # partitioning tool
+    pkgs.gnome-terminal
     pkgs.gnucash # accounting
     pkgs.gnumake # make
-    pkgs.gopher
-    pkgs.gparted # partitioning tool
-    pkgs.gptfdisk # partitioning tool
-    pkgs.graphviz
-    pkgs.gnome-terminal
-    pkgs.gnumake
     pkgs.gnupg
-    pkgs.go
-    pkgs.google-chrome
-    pkgs.guvcview # for recording from a webcam
-    pkgs.haskell-language-server
+    #pkgs.go
+    #pkgs.google-chrome
+    pkgs.graphviz
+    #pkgs.guvcview # for recording from a webcam
+    (pkgs.haskell-language-server.override { supportedGhcVersions = ["8107" "901" "921"]; })
     #pkgs.hindent
     pkgs.hlint
     pkgs.hpack
     pkgs.htop
     pkgs.imagemagick7 # CLI image editor
     pkgs.inkscape # SVG editor
-    pkgs.intellij # IntelliJ IDEa
+    #pkgs.intellij # IntelliJ IDEa
     pkgs.inxi
     pkgs.iw # wireless network manager
-    pkgs.jmtpfs # Android filesystem mounting
+    #pkgs.jmtpfs # Android filesystem mounting
     pkgs.jq
     pkgs.kcolorchooser
-    pkgs.kdenlive
+    #pkgs.kdenlive
     pkgs.konsole
     pkgs.libnotify
     pkgs.libreoffice
     pkgs.lsof # list open files
-    pkgs.lynx
+    #pkgs.lynx
     pkgs.man
     pkgs.manpages
     #pkgs.meld # diff tool
-    pkgs.minecraft
+    #pkgs.minecraft
     pkgs.mlt
-    pkgs.moc # "music on console" CLI music player
+    #pkgs.moc # "music on console" CLI music player
     pkgs.moreutils
     pkgs.mpv # video player
     pkgs.mtools
@@ -102,12 +101,12 @@
     pkgs.nix-prefetch-scripts
     #pkgs.nix-repl # REPL for the Nix language
     #pkgs.nixops # the Nix ops tool
-    pkgs.nodejs # javascript repl
-    pkgs.obs-studio
+    #pkgs.nodejs # javascript repl
+    #pkgs.obs-studio
     pkgs.okular # PDF viewer
     pkgs.ormolu
     pkgs.pandoc
-    pkgs.pandora # internet music
+    #pkgs.pandora # internet music
     pkgs.pass
     pkgs.pciutils
     pkgs.pdftk
@@ -115,14 +114,14 @@
     pkgs.pssh
     pkgs.python3
     pkgs.python3Packages.ipython # Python REPL
-    pkgs.rlwrap
-    pkgs.rip-cd
+    #pkgs.rlwrap
+    #pkgs.rip-cd
     pkgs.rsync
     pkgs.sassc
     pkgs.scrot # screenshot tool
     pkgs.silver-searcher # the "ag" command
     #pkgs.solaar
-    pkgs.sshfsFuse # SSH mount
+    #pkgs.sshfsFuse # SSH mount
     pkgs.socat
     pkgs.stack # Haskell build tool
     #pkgs.stack2nix
@@ -131,21 +130,21 @@
     #pkgs.sws
     pkgs.system-config-printer
     pkgs.telnet
-    pkgs.tesseract # OCR
+    #pkgs.tesseract # OCR
     (pkgs.texlive.combine { inherit (pkgs.texlive) scheme-small; })
     # pkgs.text-replace
     pkgs.tmux
     pkgs.totem
-    pkgs.transmission_gtk # bittorrent
+    #pkgs.transmission_gtk # bittorrent
     pkgs.tree
     pkgs.wordlist # password generator
     pkgs.usbutils
-    pkgs.unetbootin
+    #pkgs.unetbootin
     pkgs.unzip
     pkgs.vim # text editor
     pkgs.vscode
     pkgs.wget
-    pkgs.wine
+    #pkgs.wine
     pkgs.which
     pkgs.woff2
     pkgs.vscode
