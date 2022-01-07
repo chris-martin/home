19b3c6f27d6117301e15fa67437d5b977a652207
Chris Martin <ch.martin@gmail.com>
2022-01-07 03:16:59 -0700

    firefox only, no chrome

--- nix/os/web-browsers.nix
+++ nix/os/web-browsers.nix
@@ -2,9 +2,8 @@
 {
   environment.systemPackages = [
     pkgs.firefox
-    pkgs.google-chrome
   ];
   services.xserver.displayManager.sessionCommands = ''
-    xdg-settings set default-web-browser chromium.desktop
+    xdg-settings set default-web-browser firefox.desktop
   '';
 }
