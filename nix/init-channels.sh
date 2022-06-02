11042f0bcaf51fac6bcb573f2f0baf5a19f84064
Chris Martin <ch.martin@gmail.com>
2022-06-02 10:49:46 -0600

    nixos 22.05

--- nix/init-channels.sh
+++ nix/init-channels.sh
@@ -6,3 +6,4 @@ nix-channel --add https://nixos.org/channels/nixos-20.03 nixos-20.03
 nix-channel --add https://nixos.org/channels/nixos-20.09 nixos-20.09
 nix-channel --add https://nixos.org/channels/nixos-21.05 nixos-21.05
 nix-channel --add https://nixos.org/channels/nixos-21.11 nixos-21.11
+nix-channel --add https://nixos.org/channels/nixos-22.05 nixos-22.05
