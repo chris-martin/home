cdaf8e618249746adde5e3b3c53c7634e00eaade
Chris Martin <ch.martin@gmail.com>
2022-07-03 13:36:57 -0600

    ghc 9.0.2

--- nix/haskell/default.nix
+++ nix/haskell/default.nix
@@ -2,7 +2,7 @@
 
 let
 
-  compilerVersion = "ghc8107";
+  compilerVersion = "ghc902";
   packages = import ./packages.nix;
   haskellPackages = haskell.packages.${compilerVersion}.override { overrides = import ./overrides.nix haskell; };
 
