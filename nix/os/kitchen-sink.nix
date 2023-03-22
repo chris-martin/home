3edc12c7a3fc3bab38b51f3d56954aa394355308
Chris Martin <ch.martin@gmail.com>
2023-03-21 22:48:31 -0600

    haskell fiddling

--- nix/os/kitchen-sink.nix
+++ nix/os/kitchen-sink.nix
@@ -6,14 +6,14 @@ let
       pkgs = unstable;
       compilers =
         [
-          (pkgs.haskell.packages.ghc94.ghcWithPackages (p: [ p.digest p.postgresql-libpq p.sdl2 p.unix p.zlib ]))
+          # (pkgs.haskell.packages.ghc94.ghcWithPackages (p: [ p.digest p.postgresql-libpq p.sdl2 p.unix p.zlib ]))
           (pkgs.haskell.packages.ghc92.ghcWithPackages (p: [ p.digest p.postgresql-libpq p.sdl2 p.unix p.zlib ]))
-          (pkgs.haskell.packages.ghc90.ghcWithPackages (p: [ p.digest p.postgresql-libpq p.unix p.zlib ]))
+          # (pkgs.haskell.packages.ghc90.ghcWithPackages (p: [ p.digest p.postgresql-libpq p.unix p.zlib ]))
           (pkgs.haskell.packages.ghc8107.ghcWithPackages (p: [ p.digest p.postgresql-libpq p.unix p.zlib ]))
         ];
 
       hls = pkgs.haskell-language-server.override {
-        supportedGhcVersions = ["810" "90" "92" "94"];
+        supportedGhcVersions = [/*"810" "90"*/ "92" "94"];
       };
     in
       compilers ++ [
