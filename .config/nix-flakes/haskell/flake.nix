689627d68bb433390c121e46f06f1010cb35d7c9
Chris Martin <ch.martin@gmail.com>
2023-03-23 00:31:36 -0600

    haskell: add vulkan

--- .config/nix-flakes/haskell/flake.nix
+++ .config/nix-flakes/haskell/flake.nix
@@ -11,7 +11,7 @@
         ghc-8-10 = pkgs.haskell.packages.ghc8107.ghcWithPackages (p: [ p.digest p.postgresql-libpq p.unix p.zlib ]);
         ghc-9-0 = pkgs.haskell.packages.ghc90.ghcWithPackages (p: [ p.digest p.postgresql-libpq p.unix p.zlib ]);
         ghc-9-2 = pkgs.haskell.packages.ghc92.ghcWithPackages (p: [ p.digest p.postgresql-libpq p.sdl2 p.unix p.zlib ]);
-        ghc-9-4 = pkgs.haskell.packages.ghc94.ghcWithPackages (p: [ p.digest p.postgresql-libpq p.sdl2 p.unix p.zlib ]);
+        ghc-9-4 = pkgs.haskell.packages.ghc94.ghcWithPackages (p: [ p.digest p.postgresql-libpq p.relude p.sdl2 p.unix p.vulkan p.vulkan-utils p.zlib ]);
 
         hls = pkgs.haskell-language-server.override { supportedGhcVersions = ["810" "90" "92" "94"]; };
 
