a2c42b09fcd1e1e7ae7f2b0f1c6a8eacb6944108
Chris Martin <ch.martin@gmail.com>
2023-03-24 16:49:03 -0600

    convert 'br' and 'st' to fish functions

--- .config/home-manager/fish.nix
+++ .config/home-manager/fish.nix
@@ -49,12 +49,45 @@
             trash = "gio trash";
         };
         functions = {
+
+            # Lists git branches in order of their last commit date, with most recent
+            # branches appearing at the bottom. Shows date, committer, and branch name.
+            br = {
+                body = ''
+                    git for-each-ref \
+                        --sort=committerdate refs/heads/ \
+                        --format='%(committerdate:short)%09%(authorname)%09%(refname:short)' \
+                        | column -t -s "$(echo -e '\t')"
+                '';
+            };
+
+            # Enhanced git status command. First just run a normal git status. Then
+            # search for diffs that have added/removed the string "TODO" and display
+            # the results. (Ideally this would only show additions, not removals.)
+            st = {
+                body = ''
+                    git status
+
+                    set IFS (echo -en "\n\b")
+                    set todo (git diff HEAD --name-only -Gtodo)
+                    if test -n "$todo"
+                        echo '#'
+                        echo '# Todo:'
+                        echo '#'
+                        set c (echo -e '\e[0;34m')
+                        set r (echo -e '\e[0m')
+                        echo "$todo" | sed "s/.*/#       $c&$r/"
+                    end
+                '';
+            };
+
             hsnix = {
                 argumentNames = "pkg";
                 body = ''
                     cabal2nix "cabal://$pkg" > "$pkg.nix"
                 '';
             };
+
             oseary = {
                 argumentNames = "command";
                 body = ''
@@ -76,6 +109,7 @@
                     end
                 '';
             };
+
             __fish_oseary_needs_command = {
                 body = ''
                     set cmd (commandline -opc)
