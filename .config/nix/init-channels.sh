1d32a0050a2f75160c7300be735025e87b4f3d74
Chris Martin <ch.martin@gmail.com>
2023-03-26 17:03:36 -0600

    don't need nix channels or nix path at all anymore

--- .config/nix/init-channels.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs-unstable
-nix-channel --add https://nixos.org/channels/nixos-22.05 nixos-22.05
-nix-channel --add https://nixos.org/channels/nixos-22.11 nixos-22.11
-nix-channel --add https://nixos.org/channels/nixos-22.11 nixos-22.11
