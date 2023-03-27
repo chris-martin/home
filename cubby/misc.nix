87c95069889e3385c1c83e206289afa98b9feeea
Chris Martin <ch.martin@gmail.com>
2023-03-27 11:29:12 -0600

    cubby flake

---
+++ cubby/misc.nix
@@ -0,0 +1,9 @@
+{ home, pkgsUnstable, localFlakes, ... }:
+{
+    nixpkgs.config.allowUnfree = true;
+    users.defaultUserShell = "/run/current-system/sw/bin/fish";
+    services.dictd.enable = true;
+    system.stateVersion = "19.09";
+    home-manager.extraSpecialArgs = { inherit pkgsUnstable localFlakes; };
+    home-manager.users.chris = home + /config.nix;
+}
