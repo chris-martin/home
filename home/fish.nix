4402640fd35297517c0bf2a46243d8be2c92529f
Chris Martin <ch.martin@gmail.com>
2023-03-27 00:40:01 -0600

    fix 'st'

--- home/fish.nix
+++ home/fish.nix
@@ -67,17 +67,17 @@
             # the results. (Ideally this would only show additions, not removals.)
             st = {
                 body = ''
-                    git status
-
-                    set IFS (echo -en "\n\b")
-                    set todo (git diff HEAD --name-only -Gtodo)
-                    if test -n "$todo"
-                        echo '#'
-                        echo '# Todo:'
-                        echo '#'
-                        set c (echo -e '\e[0;34m')
-                        set r (echo -e '\e[0m')
-                        echo "$todo" | sed "s/.*/#       $c&$r/"
+                    git status; and begin
+                        set IFS (echo -en "\n\b")
+                        set todo (git diff HEAD --name-only -Gtodo)
+                        if test -n "$todo"
+                            echo '#'
+                            echo '# Todo:'
+                            echo '#'
+                            set c (echo -e '\e[0;34m')
+                            set r (echo -e '\e[0m')
+                            echo "$todo" | sed "s/.*/#       $c&$r/"
+                        end
                     end
                 '';
             };
