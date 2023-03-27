70664753724e06a96ac0bfbd1473ae62c08f4357
Chris Martin <ch.martin@gmail.com>
2023-03-26 19:29:54 -0600

    give steam its own flake directory

---
+++ .config/nix/steam/flake.nix
@@ -0,0 +1,13 @@
+{
+    inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    outputs = { self, nixpkgs }:
+      let
+          system = "x86_64-linux";
+          pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+          inherit (pkgs) steam;
+      in
+          {
+              nixosModule = import ./nixos-module.nix { inherit steam; };
+              defaultPackage.${system} = steam;
+          };
+}
--- .config/nix/steam/nixos-module.nix
+++ .config/nix/steam/nixos-module.nix
@@ -1,6 +1,6 @@
-{ pkgs, ... }:
+{ steam }:
 {
-  environment.systemPackages = [ pkgs.steam ];
+  environment.systemPackages = [ steam ];
   hardware.opengl.driSupport32Bit = true;
   hardware.pulseaudio.support32Bit = true;
 }
