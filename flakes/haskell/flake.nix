27d59c7c1abdd95cf86e1e05e58ed53a5f24c52f
Chris Martin <ch.martin@gmail.com>
2023-05-22 00:26:01 -0600

    less haskell

--- flakes/haskell/flake.nix
+++ flakes/haskell/flake.nix
@@ -6,34 +6,21 @@
   outputs = { self, nixpkgs }:
     let pkgs = import nixpkgs { system = "x86_64-linux"; };
     in rec {
-      ghc-9-0 = pkgs.haskell.packages.ghc90.ghcWithPackages
-        (p: [ p.digest p.postgresql-libpq p.unix p.zlib ]);
-      ghc-9-2 = pkgs.haskell.packages.ghc92.ghcWithPackages
-        (p: [ p.digest p.postgresql-libpq p.unix p.zlib ]);
-      ghc-9-4 = pkgs.haskell.packages.ghc94.ghcWithPackages (p: [
-        p.digest
-        p.postgresql-libpq
-        # p.relude
-        p.sdl2
-        p.unix
-        p.zlib
-      ]);
       ghc-9-6 = pkgs.haskell.packages.ghc96.ghcWithPackages (p: [
         p.digest
-        # p.relude
         p.unix
         p.zlib
       ]);
 
       hls = pkgs.haskell-language-server.override {
-        supportedGhcVersions = [ "90" "92" "94" ];
+        supportedGhcVersions = [/* "90" "92" */ "94" "96" ];
       };
 
       cabal = pkgs.cabal-install;
 
       defaultPackage.x86_64-linux = pkgs.symlinkJoin {
         name = "haskell";
-        paths = [ ghc-9-4 ghc-9-6 ghc-9-2 ghc-9-0 hls cabal ];
+        paths = [ ghc-9-6 hls cabal ];
       };
     };
 }
