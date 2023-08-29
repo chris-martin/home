acf9047aff024aca35bacc220e3db1928465ab8a
Chris Martin <ch.martin@gmail.com>
2023-08-29 15:57:10 -0600

    enable steam

--- os/cubby/default.nix
+++ os/cubby/default.nix
@@ -52,6 +52,12 @@
     build-cores = 0;
   };
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
   powerManagement.cpuFreqGovernor = "powersave";
 
   services.hoogle.enable = true;
